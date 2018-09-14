33e4a9ddaf3dace0735e00ac91f1d77f8b529269
Jeanette Adelson <jeanette.adelson@jadelson.local>
2018-09-14 15:57:04 -0500

    list styling

--- source.js
+++ source.js
@@ -4,8 +4,6 @@ $( document ).ready(function() {
   });
 
 
-
-
 function callApi() {
     var zip = '78756'
     var country = 'us'
@@ -14,8 +12,16 @@ function callApi() {
     url:'https://api.openweathermap.org/data/2.5/forecast/daily?zip=' + zip + ',' + country + '&APPID=' + APIKEY,
     }).done(function(data) {
       console.log(data);
+      var friday = 0
+      var saturday = 1
+      var sunday = 2
+      var monday = 3
+      var tuesday = 4
+      var wednesday = 5
+      var thursday = 6
+      console.log(saturday);
     })
 };
 
-
+// https://api.openweathermap.org/data/2.5/forecast/daily?zip=78756,us&APPID=9c9fef8066d1cc44c8414b3be1666862
 
