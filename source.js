988b482b77ff9f4e23333cee3c09ac9131250ea1
Jeanette Adelson <jeanette.adelson@jadelson.local>
2018-09-17 15:55:23 -0500

    date and day

--- source.js
+++ source.js
@@ -1,10 +1,29 @@
 $( document ).ready(function() {
-    $('.container').show()
-      callApi()
-  });
+(function () {
+  var days = [
+    'Sunday', 
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+    ];
+  d = new Date(); 
+  x = d.getDay();
+  currentDay = (days[x]);
+  })();
 
+// (function () {
+//   var day = $('.day').text()
+//   if (day === currentDay) {
+//   console.log(currentDay)
+//   } else {
+//   console.log(day)
+//   }
+// })();
 
-function callApi() {
+(function () {
     var zip = '78756'
     var country = 'us'
     var APIKEY = '9c9fef8066d1cc44c8414b3be1666862'
@@ -19,9 +38,34 @@ function callApi() {
       var tuesday = 4
       var wednesday = 5
       var thursday = 6
-      console.log(saturday);
     })
-};
+})();
 
-// https://api.openweathermap.org/data/2.5/forecast/daily?zip=78756,us&APPID=9c9fef8066d1cc44c8414b3be1666862
+(function () {
+  var d = new Date();
+  var curr_date = d.getDate();
+  var curr_month = d.getMonth();
+  curr_month++;
+  var curr_year = d.getFullYear();
+  date = (curr_date + "-" + curr_month + "-" + curr_year);
+})();
+
+console.log(date)
+
+$('.day').click(function(e) {
+  e.preventDefault()
+  $('.currentDay').removeClass('currentDay');
+  $(this).addClass('currentDay');
+  console.log(this)
+});
+
+
+
+
+
+
+
+});
+
+
 
