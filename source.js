fd22ab236d59ee3217b0936632d9c95aae51638d
Jeanette Adelson <jeanette.adelson@jadelson.local>
2018-09-18 11:16:38 -0500

    days of week

--- source.js
+++ source.js
@@ -12,35 +12,56 @@ $( document ).ready(function() {
   d = new Date(); 
   x = d.getDay();
   currentDay = (days[x]);
+  return currentDay
   })();
 
-// (function () {
-//   var day = $('.day').text()
-//   if (day === currentDay) {
-//   console.log(currentDay)
-//   } else {
-//   console.log(day)
-//   }
-// })();
 
-(function () {
+(function (data) {
     var zip = '78756'
     var country = 'us'
     var APIKEY = '9c9fef8066d1cc44c8414b3be1666862'
     $.ajax({
     url:'https://api.openweathermap.org/data/2.5/forecast/daily?zip=' + zip + ',' + country + '&APPID=' + APIKEY,
     }).done(function(data) {
-      console.log(data);
-      var friday = 0
-      var saturday = 1
-      var sunday = 2
-      var monday = 3
-      var tuesday = 4
-      var wednesday = 5
-      var thursday = 6
-    })
+      var weekdays = [
+        data.list[0],
+        data.list[1],
+        data.list[2],
+        data.list[3],
+        data.list[4],
+        data.list[5],
+        data.list[6]
+      ]
+      console.log(weekdays)
+      console.log(currentDay)
+      if (currentDay === 0) {
+        return 'Friday';
+      } else if (currentDay === 1) {
+        return 'Saturday';
+      } else if (currentDay === 2) {
+        return 'Sunday';
+      } else if (currentDay === 3) {
+        return 'Monday';
+      } else if (currentDay === 4) {
+        return 'Tuesday';
+      } else if (currentDay === 5) {
+        return 'Wednesday';
+      } else {
+        return 'Thursday';
+      }
+  });
 })();
 
+// find currentDay string and match id, then set currentDay class
+
+// currentDay when selected
+$('.day').click(function(e) {
+  e.preventDefault()
+  $('.currentDay').removeClass('currentDay');
+  $(this).addClass('currentDay');
+});
+
+// to find date
 (function () {
   var d = new Date();
   var curr_date = d.getDate();
@@ -50,14 +71,6 @@ $( document ).ready(function() {
   date = (curr_date + "-" + curr_month + "-" + curr_year);
 })();
 
-console.log(date)
-
-$('.day').click(function(e) {
-  e.preventDefault()
-  $('.currentDay').removeClass('currentDay');
-  $(this).addClass('currentDay');
-  console.log(this)
-});
 
 
 
